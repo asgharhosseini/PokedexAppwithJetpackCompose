13235c4b98ccb815e71e48014c6c4f22ccf54d88
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-04 14:54:55 +0430

    add loadPokemonPaginated in PokemonListViewModel.kt

--- app/src/main/java/ir/ah/pokedexappwithjetpackcompose/ui/screen/PokemonListScreen/PokemonListViewModel.kt
+++ app/src/main/java/ir/ah/pokedexappwithjetpackcompose/ui/screen/PokemonListScreen/PokemonListViewModel.kt
@@ -2,18 +2,36 @@ package ir.ah.pokedexappwithjetpackcompose.ui.screen.PokemonListScreen
 
 import android.graphics.*
 import android.graphics.drawable.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.*
 import androidx.palette.graphics.*
 import dagger.hilt.android.lifecycle.*
+import ir.ah.pokedexappwithjetpackcompose.data.models.*
 import ir.ah.pokedexappwithjetpackcompose.repository.*
+import ir.ah.pokedexappwithjetpackcompose.util.*
+import ir.ah.pokedexappwithjetpackcompose.util.Constants.PAGE_SIZE
+import kotlinx.coroutines.*
+import java.util.*
 import javax.inject.*
 
 @HiltViewModel
-class PokemonListViewModel @Inject constructor(private val repository: PokemonRepository):ViewModel() {
+class PokemonListViewModel @Inject constructor(private val repository: PokemonRepository) :
+    ViewModel() {
+    private var curPage = 0
 
-    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit){
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    init {
+        loadPokemonPaginated()
+    }
+
+
+    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
         Palette.from(bmp).generate { palette ->
             palette?.dominantSwatch?.rgb?.let { colorValue ->
@@ -21,4 +39,43 @@ class PokemonListViewModel @Inject constructor(private val repository: PokemonRe
             }
         }
     }
+
+    fun loadPokemonPaginated() {
+        viewModelScope.launch {
+            isLoading.value = true
+
+            val result = repository.getPokemonList(PAGE_SIZE, curPage * PAGE_SIZE)
+            when (result) {
+                is Resource.Success ->{
+                    endReached.value = curPage * PAGE_SIZE >= result.data!!.count
+                    val pokedexEntries = result.data.results.mapIndexed { index, entry ->
+                        val number = if (entry.url.endsWith("/")) {
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+                        val url =
+                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+
+                    }
+                    curPage++
+
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokedexEntries
+                }
+                is Resource.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+                }
+
+            }
+
+        }
+
+
+    }
+
+
 }
